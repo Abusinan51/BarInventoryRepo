9d4b643348ed383ee1e5670cff7280cda93ddee1
akib.almahi <akib.almahi@IC-CS233-45.uc.ucalgary.ca>
2022-08-05 00:27:21 -0600

    Created output window.
    - Usage of bottles created
    - amount of bottles left after usage
    - current stock left after usage
    - estimate of usage for next week
    - comparison between left and estimate of usage to provide info to
    update stock
    - warning created for stock overload

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,8 +14,9 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader();
-			VBox root = loader.load(new FileInputStream("src/application/InputWindow.fxml"));
-			Scene scene = new Scene(root,400,400);
+
+			VBox root = loader.load(new FileInputStream("src/application/OutputWindow.fxml"));
+			Scene scene = new Scene(root,600,430);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Bar Inventory");
 			primaryStage.show();
